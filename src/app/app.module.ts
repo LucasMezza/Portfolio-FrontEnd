4c034ac8075c628d23841e10e49b57defeffd555
LucasMezza <mezza.lucas12@gmail.com>
2023-01-11 12:13:53 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { UserComponent } from './component/user/user.component';
+import { AboutMeComponent } from './component/about-me/about-me.component';
+import { ExperienceComponent } from './component/experience/experience.component';
+import { EducationComponent } from './component/education/education.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { ProyectsComponent } from './component/proyects/proyects.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProyectsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
